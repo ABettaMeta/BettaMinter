3f564f4beacb3def4b8bfe4cb62da83bfc4ee17f
Betta Nakomoto <101670196+BettaNakomoto@users.noreply.github.com>
2022-06-14 12:13:52 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,4 @@
-import { createAlchemyWeb3 } from '@alch/alchemy-web3';
-export const NFTCONTRACT = "0x7d78EDCCBb1e0793dFD3f841934ac839502e9EeC";
-export const STAKINGCONTRACT = "0xDe4D9C2b510E7B8deaa7d8f3610237a1dbF2B026";
-export const polygonscanapi = "https://api-testnet.polygonscan.com/api";
+export const NFTCONTRACT = "0xa81Fe8A98941bbeB617c777E68771CFEac73D615";
+export const bscscanapi = "https://api.bscscan.com/";
 export const moralisapi = "https://deep-index.moralis.io/api/v2/";
-export const nftpng = "https://ipfs.io/ipfs/QmavM8Zpo9bD3r4zEnhbbBLLvHyfr1YL7f1faG3ovaeSSG/";
-export const Web3Alc = createAlchemyWeb3("https://polygon-mumbai.g.alchemy.com/v2/8AX5AP2TU6G45ctsOXNt8K4Fz_BkhhZG");
+export const nftpng = "https://ipfs.io/ipfs/bafybeiane75qkuvguywqipwuoyunfoaoihrwcrx5xug5pwub5yaab3bvpi/images/";
